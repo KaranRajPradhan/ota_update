d6fffe774ec60b8695fd14b2afb307c301e448e9
karan <karanpradhan96@gmail.com>
2019-06-05 14:29:03 +0530

    main outside

--- main/main.py
+++ main/main.py
@@ -1,8 +1,12 @@
 from ota_update.main.ota_updater import OTAUpdater
+import os
 
 
 def download_and_install_update_if_available():
+    os.chdir('ota_update')
     o = OTAUpdater('https://github.com/KaranRajPradhan/ota_update')
+    o.using_network('Jaaga Startup WiFi', 'jaagajaaga')
+    o.check_for_update_to_install_during_next_reboot()
     o.download_and_install_update_if_available('Jaaga Startup WiFi', 'jaagajaaga')
 
 
